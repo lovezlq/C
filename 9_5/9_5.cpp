e29079eebd1b5411228abc4f0de46d569ea6ae06
xaviering@outlook.com <xaviering@outlook.com>
2021-02-19 17:02:34 +0800

    ok

---
+++ 9_5/9_5.cpp
@@ -0,0 +1,30 @@
+﻿#include <conio.h>
+#include <cstdlib>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	FILE* fp;
+	char ch, st[20];
+	if ((fp=fopen("a.dat","at+"))==NULL)
+	{
+		printf("connot open file!");
+		getch();
+		exit(0);
+
+		
+	}
+	printf("input a string:\n");
+	scanf_s("%s", st);
+	fputs(st, fp);
+	rewind(fp);
+	ch = fgetc(fp);
+	while (ch!=EOF)
+	{
+		putchar(ch);
+		ch = fgetc(fp);
+		printf("\n");
+		fclose(fp);
+		
+	}
+}
