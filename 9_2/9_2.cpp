68b58e545ebc4d7e0267762914c24dde15047d9f
xaviering@outlook.com <xaviering@outlook.com>
2021-02-19 13:34:18 +0800

    9.2完成

--- 9_2/9_2.cpp
+++ 9_2/9_2.cpp
@@ -3,5 +3,28 @@
 
 int main(int argc, char* argv[])
 {
-	
+	FILE *in, *out;
+	char ch, infile[10], outfile[10];
+
+	printf("Enter the infile name:\n");
+
+	scanf_s("%s", outfile);
+	if ((in=fopen(infile,"r"))==NULL)
+	{
+		printf("connot open infile\n");
+		exit(0);
+	}
+	if ((out=fopen(outfile,"w"))==NULL)
+	{
+		printf("con not open outfile \n");
+		exit(0);
+		
+	}
+	while (!feof(in))
+	{
+		fputc(fgetc(in), out);
+		fclose(in);
+		fclose(out);
+		
+	}
 }
