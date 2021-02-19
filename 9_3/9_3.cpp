af585c871a153624d93fc49c8acccf8ecf4d1f15
xaviering@outlook.com <xaviering@outlook.com>
2021-02-19 16:09:35 +0800

    9.3

---
+++ 9_3/9_3.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	FILE* fp;
+	char ch;
+	if ((fp=fopen("A.dat","r"))==NULL)
+	{
+		printf("connot open file !\n");
+		exit(0);
+		
+	}
+	while ((ch=fgetc(fp))!=EOF)
+	{
+		putchar(ch);
+		
+	}
+	fclose(fp);
+	
+}
