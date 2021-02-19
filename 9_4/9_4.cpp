970370b58dbf48dfd35bd5fa2a541720d661ee79
xaviering@outlook.com <xaviering@outlook.com>
2021-02-19 16:45:02 +0800

    9.4完成

---
+++ 9_4/9_4.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	FILE* fp;
+	char str[11];
+	if ((fp=fopen("a.dat","rt"))==NULL)
+	{
+		printf("connot open file !");
+		getch();
+		exit(0);
+		
+	}
+	fgets(str, 11, fp);
+	printf("%s", str);
+	fclose(fp);
+	
+}
